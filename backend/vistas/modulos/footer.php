e347b75bcaba41c1deab5c86723fdf82e73f5fc6
Jose Alejandro <carmens_321@outlook.com>
2018-12-14 10:09:04 -0600

    Proyecto final

---
+++ backend/vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+ <!-- main-footer -->
+<footer class="main-footer">
+
+<strong>Copyright &copy; 2018 <a href="#">TeamBlack4Ever</a>.</strong> Todos los derechos reservados.
+
+</footer>
+<!-- main-footer -->
